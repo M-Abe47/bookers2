9d3cdae147eaafcc6c7ee3ea281d1ae6cf87b470
M-Abe47 <abuzhengze5@gmail.com>
2021-06-02 13:15:41 +0000

    fixed comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,8 +5,12 @@ class BookCommentsController < ApplicationController
     comment = BookComment.new(book_comment_params)
     comment.user_id = current_user.id
     comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    if comment.save
+      redirect_to request.referer
+    else
+      flash[:notice] = "Comments cannot be left blank."
+      redirect_to request.referer
+    end
   end
 
   def destroy
